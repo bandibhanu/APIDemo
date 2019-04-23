880e64d546f9119dfab2143e45036a3eff388d29
bhanu <bhanubandi3@gmail.com>
2019-04-23 10:27:08 +0000

    Api demo

---
+++ APIAppDemo/Controllers/SimpleAPIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIAppDemo.Controllers
+{
+    public class SimpleAPIController : ApiController
+    {
+        public string Get()
+        {
+            return "welcome to the wold of API";
+        }
+        public List<string> Get(int Id)
+        {
+            return new List<string>
+            {
+                "welcome bro"
+            };
+
+        }
+        
+       
+    }
+}
